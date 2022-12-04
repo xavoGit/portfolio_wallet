ac6713b9cef9313c71d068125c9d53c9beffea89
riruiz <riruiz@host.docker.internal>
2022-12-03 21:46:58 -0600

    Remove eureka dependencies

--- src/main/java/com/xavo/finance/wallet/WalletApplication.java
+++ src/main/java/com/xavo/finance/wallet/WalletApplication.java
@@ -2,13 +2,11 @@ package com.xavo.finance.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class WalletApplication {
 
 	public static void main(String[] args) {
